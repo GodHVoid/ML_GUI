77e8d431a458998ca1b321dd0825590cf214c5b5
Renato Millan <91162242+GodHVoid@users.noreply.github.com>
2023-09-18 18:55:06 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import GuiCanvas from './components/guiCanvas/guiCanvas.js';
+import TextInANest from './components/discription/discription';
 function App() {
   return (
-    <div className='.App'>
+    <div className='App'>
+      <div className='dis'>
+      <TextInANest></TextInANest>
+      </div>
+      <div>
       <GuiCanvas ></GuiCanvas>
+      </div>
+      <div className='credit'>
+      <TextInANest></TextInANest>
+      </div>
     </div> 
   );
 }
+
 
 export default App;
